bee30368b4ba55a9d8226da9d106967a13a03e8b
Jelson J <jelson20@outlook.com>
2021-10-24 15:35:19 +0100

    commit changes styles.css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-
-
+//GALLERY FILTER
 const filterBnt = document.querySelector("#filter-btn").children;
-
 const allItems = document.querySelector(".work-gallery").children;
 
 for (let i = 0; i < filterBnt.length; i++){
@@ -9,7 +7,7 @@ for (let i = 0; i < filterBnt.length; i++){
   for (let a = 0; a < filterBnt.length; a++){
   filterBnt[a].classList.remove("active")
   }
-  this.classList.add(active)
+  this.classList.add("active")
   const target = this.getAttribute("data-target")
 
   for (let y = 0; y < allItems.length; y++){
